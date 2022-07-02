36a64adbcbc559de436926637d2fe30863fdba64
username16838 <89617918+username16838@users.noreply.github.com>
2022-07-02 19:41:37 -0400

    v1.1

--- calc.py
+++ calc.py
@@ -1,4 +1,7 @@
-# ui
+print("I have a special calculator for calculating averages")
+print("you can find it here | > https://github.com/username16838/calculator/blob/main/calc.py")
+
+print('\n')
 
 num = float(input('>>> '))
 op = input('>>>')
@@ -6,15 +9,25 @@ num1 = float(input('>>> '))
 
 # logic
 
-if op =='+':
-    sum = num + num1
-    print('>>> ' + str(sum))
-if op =='-':
-    sum = num - num1
-    print('>>> ' + str(sum))
-if op =='*':
-    sum = num * num1
-    print('>>> ' + str(sum))
-if op =='/':
-    sum = num / num1
-    print('>>> ' + str(sum))
+while True:
+    if op =='+':
+        sum = num + num1
+        print('>>> ' + str(sum))
+    if op =='-':
+        sum = num - num1
+        print('>>> ' + str(sum))
+    if op =='*':
+        sum = num * num1
+        print('>>> ' + str(sum))
+    if op =='/':
+        sum = num / num1
+        print('>>> ' + str(sum))
+    
+    print("would you like to run again | y/n")
+
+    run_again = input('>>> ')
+
+    if run_again =='n':
+        break
+    else:
+        continue
